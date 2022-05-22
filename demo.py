20b2b8efe4d49fca77589cae45c1d50b54ac83f0
atang816 <alec.tang@sjsu.edu>
2022-05-21 17:17:30 -0700

    Added database insertion

--- demo.py
+++ demo.py
@@ -1,30 +1,109 @@
 import requests
 from bs4 import BeautifulSoup
-import base64
+import psycopg2
+from config import config
+
+def getNames(soup):
+    shoes_list = []
+    # Get shoes name
+    shoes_grid = soup.find_all('div', class_='GridCellProductInfo__Name-sc-17lfnu8-3 hUVYBh')
 
 
-result = requests.get("https://www.goat.com/brand/air-jordan") # stockx.com is blocked
-soup = BeautifulSoup(result.content, "lxml")
+    for shoe_name in shoes_grid:
+        shoes_list.append(shoe_name.text)
+        print(shoe_name.text)
 
-shoes_list = []
-# Get shoes name
-shoes_grid = soup.find_all('div', class_='GridCellProductInfo__Name-sc-17lfnu8-3 dwAEbs')
+    return shoes_list
 
+def getPrices(soup):
+    prices_list = []
+    shoes_prices = soup.find_all('div', class_="GridCellProductInfo__Price-sc-17lfnu8-6 KlQNy")
 
-for shoe_name in shoes_grid:
-    shoes_list.append(shoe_name.text)
-    print(shoe_name.text)
+    for shoe_price in shoes_prices:
+        prices_list.append(shoe_price.text)
+        print(shoe_price.text)
 
-shoes_prices = soup.find_all('div', class_="GridCellProductInfo__Price-sc-17lfnu8-6 tIvOr")
+    return prices_list
 
-for shoe_price in shoes_prices:
-    print(shoe_price.text)
+def connect():
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # read connection parameters
+        params = config()
 
-#
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params)
+
+        # create a cursor
+        cur = conn.cursor()
+
+        # execute a statement
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
+
+        # close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+def insert_shoe(name, price, image):
+    """ insert a new vendor into the vendors table """
+
+    sql = """INSERT INTO shoes (name, price, image) VALUES(%s, %s, %s) RETURNING shoes.id;"""
+    conn = None
+    shoes_id = None
+    try:
+        # read database configuration
+        params = config()
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the INSERT statement
+        without_dollar_sign = price.replace('$', "")
+        cur.execute(sql, (name,int(without_dollar_sign),image,))
+        # get the generated id back
+        shoes_id = cur.fetchone()[0]
+        # commit the changes to the database
+        conn.commit()
+        # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return shoes_id
+
+if __name__ == '__main__':
+
+    result = requests.get("https://www.goat.com/brand/air-jordan")  # stockx.com is blocked
+    soup = BeautifulSoup(result.content, "lxml")
+
+    names_list = getNames(soup)
+    prices_list = getPrices(soup)
+    connect()
+
+    print(names_list[0])
+    print(prices_list[0])
+
+    insert_shoe(names_list[2], prices_list[2], "none")
+
 # shoes_images = soup.find_all('img')
 #
 # for shoe_image in shoes_images:
-#     print(shoe_image.attrs["src"])
+#      print(shoe_image.attrs["src"])
 
 #links = soup.find_all("a") # Print all the links of page
 
@@ -32,12 +111,3 @@ for shoe_price in shoes_prices:
 #      if "Jordan" in link.text:
 #          print(link)
 #          print(link.attrs['href'])
-
-
-#<div data-qa="grid_cell_product_price" class="GridCellProductInfo__Price-sc-17lfnu8-6 tIvOr"><span>$230</span></div>
-
-#<img alt="Air Jordan 5 Retro 'Racer Blue'"
-# data-qa="grid_cell_product_image"
-# sizes="(min-width: 768px) 25vw, 50vw"
-# class="sc-eCApnc intbbb Image__StyledImage-sc-1qwz99p-0 gJfnCb GridCellProductImage__Image-msqmrc-1 efCEgn"
-# src="https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=750" srcset="https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=50 50w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=75 75w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=100 100w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=120 120w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=150 150w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=200 200w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=240 240w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=300 300w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=360 360w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=375 375w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=400 400w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=500 500w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=600 600w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=700 700w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=750 750w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=800 800w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=850 850w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=900 900w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=950 950w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=1000 1000w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=1250 1250w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=1500 1500w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=1600 1600w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=1800 1800w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=2000 2000w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=2200 2200w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=2400 2400w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=2600 2600w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=3000 3000w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=3200 3200w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=3500 3500w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=3700 3700w, https://image.goat.com/transform/v1/attachments/product_template_pictures/images/066/685/865/original/824865_00.png.png?action=crop&amp;width=4000 4000w">
---
+++ image_search.py
@@ -0,0 +1,10 @@
+from google_images_download import google_images_download
+
+#instantiate the class
+response = google_images_download.googleimagesdownload()
+arguments = {"keywords":"aeroplane, school bus, dog in front of house",
+             "limit":10,"print_urls":False}
+paths = response.download(arguments)
+
+#print complete paths to the downloaded images
+print(paths)
